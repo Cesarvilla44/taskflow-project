7f95a145d26d4b3f0f1e00edcf623d55933ddef8
Cesar <cesar.villacanas@alu.ceacfp.es>
2026-03-16 11:21:24 +0100

    Creación del app.get para que el servidor haga algo cuando se abre por primera vez

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,10 @@
 const { PORT } = require('./config/env');
 const express = require('express');
 const app = express();
+app.get('/', (req, res) => {
+    res.send('¡Hola! El servidor está funcionando perfectamente 🚀');
+});
+// -----------------------
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`);
