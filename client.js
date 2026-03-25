2cdd96f6b1dd4003e6d9d36d74cea538c1c2ca8e
Cesar <cesar.villacanas@alu.ceacfp.es>
2026-03-25 11:22:26 +0100

    Cambios en Vercel.json

--- client.js
+++ client.js
@@ -1,6 +1,6 @@
 // Usamos el axios que acabamos de cargar en el HTML
 const client = axios.create({
-  baseURL: 'https://taskflow-project-orpin.vercel.app/api/v1',
+  baseURL: 'https://taskflow-project-orpin.vercel.app/api',
 });
 
 export default client;
